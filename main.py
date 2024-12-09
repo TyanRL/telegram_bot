4f2f8e9dca7cef29d1e6b54be97a7bff07758586
Roman Tyan <Roman.Tyan@monitel.ru>
2024-12-09 16:15:27 +0300

    weather

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ from sql import get_admins, in_user_list
 from yandex_maps import get_address
 
 
-version="5.8"
+version="5.9"
 
 # Инициализация OpenAI и Telegram API
 opena_ai_api_key=os.getenv('OPENAI_API_KEY')
