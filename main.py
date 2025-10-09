6c76fb8abc247af2b6c8fa9cbd7209aab8d20c08
TyanRL <tnrom@yandex.ru>
2025-10-09 07:04:45 +0300

    gpt-5

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ from sql import get_admins, in_user_list
 from yandex_maps import get_address
 
 
-version="10.36"
+version="11.00"
 
 # Инициализация OpenAI и Telegram API
 opena_ai_api_key=os.getenv('OPENAI_API_KEY')
